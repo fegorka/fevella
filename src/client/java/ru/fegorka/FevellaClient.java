7d75fbadfd149d5a7fbd20145e23ded0e24f3c1d
fegorka <101581584+fegorka@users.noreply.github.com>
2023-12-28 20:30:28 +0300

    build(initial): added base mod sctructure

---
+++ src/client/java/ru/fegorka/FevellaClient.java
@@ -0,0 +1,10 @@
+package ru.fegorka;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FevellaClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/main/java/ru/fegorka/FevellaDataGenerator.java
@@ -0,0 +1,11 @@
+package ru.fegorka;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class FevellaDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
